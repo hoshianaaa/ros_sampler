2fbda2a60b0ba6ff463d1b63cfc6882e22ed0d72
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-26 21:55:12 +0900

    update

---
+++ scripts/psp_sample.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import *
+from geometry_msgs.msg import *
+
+import os,sys
+sys.path.append(os.path.join(os.path.dirname(__file__), './python_utils'))
+sys.path.append(os.path.join(os.path.dirname(__file__), './python_ros_utils'))
+
+from python_utils import *
+from python_ros_utils import *
+from psp import PSP
+
+if __name__ == '__main__':
+
+  node_name = "param_sub_pub"
+  rospy.init_node(node_name)
+
+  psp_list = []
+
+  for i in range(10):
+    psp_list.append(PSP(node_name + "/test_data" + str(i)))
+
+  r = rospy.Rate(10)
+  while not rospy.is_shutdown():
+    for i in range(10):
+      print(psp_list[i].process())
+    r.sleep()
+
