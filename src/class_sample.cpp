4c56c4fe7c0d298ad7335f6524f458830e1aee4f
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-27 23:49:20 +0900

    update

--- src/class_sample.cpp
+++ src/class_sample.cpp
@@ -80,7 +80,7 @@ int main(int argc, char * argv[])
     Basic node = Basic(&nh);
     PSP psp = PSP(&nh,"psp_sample/mode");
 
-    ros::Rate loop_rate(1);
+    ros::Rate loop_rate(100);
 
     while (ros::ok())
     {
