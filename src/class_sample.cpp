f7c43110c1f8429ca0d00266711036675eb7ceb4
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-28 00:08:36 +0900

    update

--- src/class_sample.cpp
+++ src/class_sample.cpp
@@ -3,6 +3,7 @@
 #include"std_msgs/String.h"
 #include <iostream>
 
+
 class PSP
 {
     public:
@@ -42,6 +43,68 @@ class PSP
         std::string data_, name_;
 };
 
+std::string list2string(std::vector<std::string> list)
+{
+  std::string ret;
+  int size = list.size();
+  for (int i=0;i<size;i++)
+  {
+    ret += list[i];
+    if (i!=size-1)
+      ret += ",";
+  }
+  return ret;
+}
+
+class PSP_mode
+{
+    public:
+        PSP_mode(ros::NodeHandle *nodehandle, std::string name, std::vector<std::string> mode_list):nh_(*nodehandle)
+         {
+            pub = nh_.advertise<std_msgs::String>(name + "_state", 1);
+            list_pub = nh_.advertise<std_msgs::String>(name + "_list", 1);
+            sub = nh_.subscribe(name, 1000, &PSP_mode::callback, this);
+            timer = nh_.createTimer(ros::Duration(0.1), &PSP_mode::main_loop, this);
+
+            data_ = mode_list[0];
+            nh_.getParam(name,data_);
+
+            name_ = name;
+            mode_list_ = mode_list;
+         }
+
+          void callback(const std_msgs::String& msg)
+         {
+           data_ = msg.data;
+           nh_.setParam(name_,data_);
+         }
+
+         void main_loop(const ros::TimerEvent &)
+         {
+             std_msgs::String msg;
+             msg.data = data_;
+             pub.publish(msg);
+
+             std_msgs::String msg2;
+             msg2.data = list2string(mode_list_);
+             list_pub.publish(msg);
+         }
+
+         std::string get_value()
+         {
+            return data_;
+         }
+
+
+    private:
+        ros::NodeHandle nh_;
+        ros::Publisher pub, list_pub;
+        ros::Subscriber sub;
+        ros::Timer timer;
+        std::string data_, name_;
+        std::vector<std::string> mode_list_;
+
+};
 
 class Basic
 {
@@ -78,13 +141,17 @@ int main(int argc, char * argv[])
     ros::init(argc, argv, "nodename");
     ros::NodeHandle nh;
     Basic node = Basic(&nh);
-    PSP psp = PSP(&nh,"psp_sample/mode");
+    PSP psp = PSP(&nh,"psp_sample/psp");
+
+    std::vector<std::string> mode_list = {"mode1","mode2","mode3"};
+    PSP_mode psp_mode = PSP_mode(&nh,"psp_sample/mode", mode_list);
 
     ros::Rate loop_rate(100);
 
     while (ros::ok())
     {
       std::cout << "psp val: " << psp.get_value() << std::endl;
+      std::cout << "psp mode val: " << psp_mode.get_value() << std::endl;
       ros::spinOnce();
       loop_rate.sleep();
     }
